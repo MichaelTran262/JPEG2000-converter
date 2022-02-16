339da51495a39ac3eb22d2db7db08f1c6328d107
michaeltran262 <michaeltran262@gmail.com>
2022-02-16 16:00:59 +0100

    Added database communication

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import os
+import zipfile
 
 from flask import Flask, request, render_template, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 from flask_dropzone import Dropzone
-import shutil
+
+from datetime import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-
 app.config.update(
-    UPLOADED_PATH=os.path.join(basedir, 'tmp'),
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@db:5432/image_database',
+    UPLOADED_PATH=os.path.join(basedir, 'tmp/jp2/'),
+    CONVERTED_PATH=os.path.join(basedir, 'tmp/converted/'),
     # Flask-Dropzone config:
     DROPZONE_ALLOWED_FILE_CUSTOM=True,
     DROPZONE_ALLOWED_FILE_TYPE='.jp2',
@@ -20,47 +24,94 @@ app.config.update(
     DROPZONE_UPLOAD_ACTION='handle_upload',  # URL or endpoint
     DROPZONE_UPLOAD_BTN_ID='submit',
 )
-
+db = SQLAlchemy(app)
 dropzone = Dropzone(app)
 
-@app.route('/')
+class Batch(db.Model):
+    __tablename__ = "batch"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    path = db.Column(db.String(64), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    children = db.relationship("Image")
+
+class Image(db.Model):
+    __tablename__ = "image"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    size = db.Column(db.Integer, nullable=True)
+    batch_id = db.Column(db.Integer, db.ForeignKey('batch.id'))
+    
+    
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    filenames = os.listdir('tmp2/')
-    return render_template('index.html', files=filenames)
+    if request.method == 'POST':
+        #shutil.make_archive('batch/' + title, 'zip', os.path.join(basedir, 'tmp2'))
+        archives = Batch.query.order_by(Batch.date_created).all()
+        return render_template('index.html', files=archives)
+    else:    
+        #archives = os.listdir('batch/')
+        archives = Batch.query.order_by(Batch.date_created).all()
+        return render_template('index.html', files=archives)
 
 
 @app.route('/upload', methods=['POST'])
 def handle_upload():
     if request.method == 'POST':
-        title = request.form.get('title')
         for key, f in request.files.items():
             if key.startswith('file'):
                 f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
                 from_file = os.path.join(app.config['UPLOADED_PATH'], f.filename)
                 filename = os.path.splitext(f.filename)[0]
-                to_file = os.path.join(basedir, 'tmp2') + '/' + filename + '.tif'
+                to_file = os.path.join(basedir, 'tmp') + '/converted/' + filename + '.tif'
                 try:
                     if os.system('vips copy ' + from_file + ' ' + to_file) != 0:
-                        raise Exception('Libs did not work')
+                        raise Exception('Command vips did not work!')
                 except:
                     os.system('opj_decompress -i ' + from_file + ' -o ' + to_file)
-                    print("command does not work")
-    return '', 204
+                    print("Command opj_decompress does not work")   
+        title = request.form.get('title')
+        zip_path = os.path.join(basedir, 'batch/'+title+'.zip')
+        zipf = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
+        path = os.path.join(basedir, 'tmp/converted/')
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                zipf.write( os.path.join(root, file),
+                            os.path.relpath(os.path.join(root, file), path))
+        zipf.close()
+        for file in os.listdir(app.config['UPLOADED_PATH']):
+            os.remove(app.config['UPLOADED_PATH'] + file)
+        for file in os.listdir(app.config['CONVERTED_PATH']):
+            os.remove(app.config['CONVERTED_PATH'] + file)  
+        filenames = os.listdir('batch/')
+        zip_name = title + '.zip'
+        new_zip = Batch(name=zip_name, path=zip_path)
+        try:
+            db.session.add(new_zip)
+            db.session.commit()
+            archives = Batch.query.order_by(Batch.date_created).all()
+            return render_template('index.html', files=archives)
+        except:
+            return 'Error during db insertion', 500
+    else:
+        return '', 204
 
-@app.route('/', methods=['POST'])
-def handle_form():
-    title = request.form.get('title')
-    #shutil.make_archive('batch/' + title, 'zip', os.path.join(basedir, 'tmp2'))
-    filenames = os.listdir('tmp2/')
-    return render_template('index.html', files=filenames)
-
-@app.route('/tmp2/<path:filename>', methods=['GET', 'POST'])
+@app.route('/batch/<path:filename>', methods=['GET', 'POST'])
 def log(filename):
+    return send_from_directory(
+        os.path.abspath('batch'),
+        filename,
+        as_attachment=True
+    )
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def log2(filename):
     return send_from_directory(
         os.path.abspath('tmp2'),
         filename,
         as_attachment=True
     )
+
 
 
 if __name__ == "__main__":
